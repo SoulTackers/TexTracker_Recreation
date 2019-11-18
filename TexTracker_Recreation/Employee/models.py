9ba9d0650bfcec208e78fa77bcb43b2f8007c5e1
MrMindHacker <sonarkeyur@gmail.com>
2019-11-19 03:57:30 +0530

    customuser, profile and permission decorators added

--- TexTracker_Recreation/Employee/models.py
+++ TexTracker_Recreation/Employee/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from User.models import CustomUser
+from phone_field import PhoneField
+
+
+class EmployeePost(models.Model):
+    ep_id = models.AutoField(primary_key=True)
+    ep_details = models.TextField(blank=True, null=True)
+    ep_name = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return self.ep_name
+
+    class Meta:
+        managed = True
+        db_table = 'employee_post'
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, blank=True, null=True)
+    employee_id = models.AutoField(primary_key=True)
+    employee_name = models.CharField(max_length=256, blank=True, null=True)
+    employee_postid = models.ForeignKey(
+        EmployeePost, on_delete=models.CASCADE, blank=True, null=True)
+    employee_phone = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.employee_name
+
+    class Meta:
+        managed = True
+        db_table = 'employee'
--- TexTracker_Recreation/User/admin.py
+++ TexTracker_Recreation/User/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from User.models import CustomUser, CustomUserProfile, Role
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Role)
+admin.site.register(CustomUserProfile)
